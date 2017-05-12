dffac61fa5edcbe830a95f6acdaf28bee2b1bfa9
yulijunzj <yulijunzj@zju.edu.cn>
2017-05-12 17:47:56 +0800

    网格索引构建

--- src/main/java/org/gislab/hbase/index/IndexFactory.java
+++ src/main/java/org/gislab/hbase/index/IndexFactory.java
@@ -4,8 +4,12 @@ package org.gislab.hbase.index;
  * Created by ladyfish on 2017/5/12.
  */
 public class IndexFactory {
-    public static GridIndex getGridIndex(){
-        return new GridIndex();
+    public static GridIndex getGridIndex(double tlX,double tlY,double px,double py){
+        return new GridIndex(tlX,tlY,px,py);
+    }
+
+    public static GridIndex getGridIndex(double tlX,double tlY,double px,double py,int xSize,int ySize){
+        return new GridIndex(tlX,tlY,px,py,xSize,ySize);
     }
     public static MutiGridIndex getMutiGridIndex(){
         return new MutiGridIndex();
